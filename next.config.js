0c510cb6cfc1e6805157c4030ccfd880369b98a0
Shubham kanodia <shubham.kanodia@urbanladder.com>
2017-09-08 20:41:04 +0530

    Use API endpoint in prod'

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const glob = require('glob')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? 'bundlephobia' : '',
+  assetPrefix: isProd ? '/bundlephobia' : '',
   exportPathMap: function () {
     return {
       '/': { page: '/' },
