b7594241966c944fec0a35451690d912492d75c3
Shubham Kanodia <shubham.kanodia10@gmail.com>
2017-09-10 19:42:47 +0530

    Handle MissingDependencyError

--- server/CustomError.js
+++ server/CustomError.js
@@ -4,8 +4,9 @@
  */
 // Use ES6 supported by Node v6.10 only!
 
-module.exports = function CustomError(name, originalError) {
+module.exports = function CustomError(name, originalError, extra) {
   Error.captureStackTrace(this, this.constructor)
   this.name = name
   this.originalError = originalError
+  this.extra = extra
 }
