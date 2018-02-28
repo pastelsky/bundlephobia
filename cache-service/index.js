72807225f5bc7fd53bcbd51383a3d914749d657a
Shubham Kanodia <shubham.kanodia10@gmail.com>
2018-02-28 23:53:27 +0530

    Switch from server to fastify

--- cache-service/index.js
+++ cache-service/index.js
@@ -1,18 +1,15 @@
 require('dotenv').config()
-require('now-logs')('bundlephobia')
 const firebase = require('firebase')
 const LRU = require('lru-cache')
 const debug = require('debug')('bp:cache')
-const server = require('server')
-const { status } = server.reply;
-const { get, post } = server.router;
+const fastify = require('fastify')()
 
-const LRUCache = LRU({ max: 10000 })
+const LRUCache = LRU({ max: 5000 })
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 }
 
 firebase.initializeApp(firebaseConfig)
@@ -45,7 +42,7 @@ async function getPackageResult({ name, version }) {
   return snapshot.val()
 }
 
-async function setPackageResult({ name, version, result}) {
+async function setPackageResult({ name, version, result }) {
   const modules = firebase.database().ref().child('modules-v2')
   return modules
     .child(encodeFirebaseKey(name))
@@ -72,46 +69,48 @@ try {
   console.log('Load error: ', err)
 }
 
-server({ port: 7000, security: { csrf: false } }, [
-  get('/cache', async ctx => {
-    const name = decodeURIComponent(ctx.query.name)
-    const version = decodeURIComponent(ctx.query.version)
+fastify.get('/cache', async (req, res) => {
+  const name = decodeURIComponent(req.query.name)
+  const version = decodeURIComponent(req.query.version)
 
-    if (!name || !version) {
-      return status(422)
+  if (!name || !version) {
+    return res.code(422).send()
+  }
+  debug('get %s@%s', name, version)
+  const lruCacheEntry = LRUCache.get(`${name}@${version}`)
+
+  if (lruCacheEntry) {
+    debug('cache hit: memory')
+    return res.code(200).send(lruCacheEntry)
+  } else {
+    const result = await getPackageResult({ name, version })
+    if (result) {
+      debug('cache hit: firebase')
+      LRUCache.set(`${name}@${version}`, result)
+      return res.code(200).send(result)
     }
-    debug('get %s@%s', name, version)
-    const lruCacheEntry = LRUCache.get(`${name}@${version}`)
+  }
+  return res.code(404).send()
+})
 
-    if (lruCacheEntry) {
-      debug('cache hit: memory')
-      return status(200).send(lruCacheEntry)
-    } else {
-      const result = await getPackageResult({ name, version })
-      if (result) {
-        debug('cache hit: firebase')
-        LRUCache.set(`${name}@${version}`, result)
-        return status(200).send(result)
-      }
-    }
-    return status(404)
-  }),
+fastify.post('/cache', async (req, res) => {
+  const { name, version, result } = req.body
 
-  post('/cache', async ctx => {
-    const { name, version, result } = ctx.data
+  if (!name || !version || !result)
+    return res.code(422).send()
 
-    if (!name || !version || !result)
-      return status(422)
+  debug('set %O to %O', { name, version }, result)
+  LRUCache.set(`${name}@${version}`, result)
+  try {
+    await setPackageResult({ name, version, result })
+    return res.code(201).send()
+  } catch (err) {
+    console.log(err)
+    return res.code(500).send({ error: err })
+  }
+})
 
-    debug('set %O to %O', { name, version }, result)
-    LRUCache.set(`${name}@${version}`, result)
-    try {
-      await setPackageResult({ name, version, result })
-      return status(201)
-    } catch(err) {
-      console.log(err)
-      return status(500).send({ error: err })
-    }
-  })
-]);
-
+fastify.listen(7001, '127.0.0.1', function (err) {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
