e44878add52135fdc94c09acc1fad614d4b0fd89
Shubham Kanodia <shubham.kanodia10@gmail.com>
2018-10-07 16:49:00 +0530

    v0.0.2

---
+++ server/data/similar-packages/index.js
@@ -0,0 +1,10 @@
+import dateTimeList from './date-time'
+import markdownList from './markdown'
+import storageList from './storage'
+
+
+export default [
+  ...dateTimeList,
+  ...markdownList,
+  ...storageList,
+]
