d5415695fef9536afbdf54af93fea48ac3ab4a46
Shubham Kanodia <shubham.kanodia10@gmail.com>
2018-10-22 00:35:24 +0530

    Split build endpoint into multiple middlewares

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 // Use ES6 supported by Node v6.10 only!
 
 const path = require('path')
+const dev = false //process.env.NODE_ENV === 'development'
 
 module.exports = {
   tmp: path.join(__dirname, '..', 'tmp-build'),
@@ -17,6 +18,19 @@ module.exports = {
     /hack-cheats/,
     /hacks?-cheats?/,
     /hack-unlimited/,
+    /generator-unlimited/,
     /hack-\d+/,
+    /cheat-\d+/,
+    /-hacks?-/,
   ],
+  CACHE: {
+    PUBLIC_ASSETS: dev ? 0 : 24 * 60 * 60,
+    RECENTS_API: dev ? 0 : 20 * 60,
+    PACKAGE_HISTORY_API: dev ? 0 : 60 * 60,
+    SIMILAR_API: dev ? 0 : 60 * 60 * 2,
+    SIZE_API_DEFAULT: dev ? 0 : 30,
+    SIZE_API_ERROR: dev ? 0 : 60,
+    SIZE_API_ERROR_FATAL: dev ? 0 : 60 * 60,
+    SIZE_API_HAS_VERSION: dev ? 0 : 12 * 60 * 60,
+  }
 }
