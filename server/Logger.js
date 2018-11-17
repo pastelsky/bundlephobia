645b6b5dcd0a801fb79eb1b4ece048cb10095645
Shubham Kanodia <shubham.kanodia10@gmail.com>
2018-11-17 23:38:07 +0530

    Refactored logging a bit

--- server/Logger.js
+++ server/Logger.js
@@ -1,30 +1,42 @@
 const winston  = require('winston');
-const logzioWinstonTransport = require('winston-logzio');
+const LogzioWinstonTransport = require('winston-logzio');
+const { SumoLogic } = require('winston-sumologic-transport');
+const SumoLogger = require('sumo-logger');
+
+
+const sumoLogger = new SumoLogger({
+  endpoint: process.env.SUMOLOGIC_URL,
+});
 
 class Logger {
   constructor() {
-    winston.add(logzioWinstonTransport, {
+    const logzioTransport = new LogzioWinstonTransport({
+      name: 'bundlephobia',
       token: process.env.LOGZIO_TOKEN,
-      host: 'listener.logz.io',
+    })
+
+    const sumologicTransport = new SumoLogic({
+      url: process.env.SUMOLOGIC_URL,
     });
 
     winston.remove(winston.transports.Console);
-    this.logger = winston
+    this.logger = winston.createLogger({
+      transports: process.env.NODE_ENV !== 'production' ? [] : [logzioTransport, sumologicTransport]
+    });
   }
 
   info(tag, json, message) {
-    // console.log('JSON', json)
     this.logger.log('info', message, {
-      type: tag,
-      message: json
+      tag,
+      ...json
     })
   }
 
   error(tag, json, message) {
-    // console.log('JSON', json)
+    console.log('JSON', json)
     this.logger.log('error', message, {
-      type: tag,
-      message: json
+      tag,
+      ...json
     })
   }
 }
