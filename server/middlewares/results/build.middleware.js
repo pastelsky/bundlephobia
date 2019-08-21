06cca04566d47cbefbb64dbf9b0fce6f6c2cebb0
Shubham Kanodia <shubham.kanodia10@gmail.com>
2019-08-21 22:34:50 +0530

    Fix logging of circular JSON

--- server/middlewares/results/build.middleware.js
+++ server/middlewares/results/build.middleware.js
@@ -28,7 +28,7 @@ requestQueue.setExecutor(async ({ packageString, name }) => {
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
         debug('No response received from build server. Is the server down?')
-        throw new CustomError('BuildError', error.request)
+        throw new CustomError('BuildError', { reason: 'BUILD_SERVICE_UNREACHABLE', url: error.request._currentUrl })
       } else {
         // Something happened in setting up the request that triggered an Error
         throw new CustomError('BuildError', error.message)
