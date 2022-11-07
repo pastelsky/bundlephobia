49b1b76261754d05656b473c434ee99aa87fa816
Ilya Daraseliya <idarase@gmail.com>
2022-11-07 09:39:16 +0100

    enable compression

--- index.ts
+++ index.ts
@@ -96,6 +96,9 @@ app.prepare().then(() => {
         return /(text|json|javascript|svg)/.test(contentType)
       },
       threshold: 2048,
+      gzip: {
+        flush: require('zlib').Z_SYNC_FLUSH,
+      },
     })
   )
 
