1d431b37797f44725b1201b898c9e005afa91d79
Tianning Li <ltianningli@gmail.com>
2023-01-09 17:11:36 -0800

    Add SimilarPackagesSection typescript migration

---
+++ pages/package/[...packageString]/components/SimilarPackagesSection/SimilarPackagesSection.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import SimilarPackageCard from '../../../../../client/components/SimilarPackageCard/SimilarPackageCard'
+
+interface SimilarPackagesSectionProps {
+  packs: any[]
+  category: string
+  comparisonGzip: number
+}
+
+const SimilarPackagesSection: React.FC<SimilarPackagesSectionProps> = ({
+  packs,
+  category,
+  comparisonGzip,
+}) => {
+  return (
+    <div className="similar-packages-section">
+      <h2 className="result__section-heading similar-packages-section__heading">
+        {' '}
+        Similar Packages{' '}
+      </h2>
+      <h5 className="similar-packages-section__subheading"> {category} </h5>
+
+      <div className="similar-packages-section__list">
+        {packs.map(pack => (
+          <SimilarPackageCard
+            key={pack.name}
+            pack={pack}
+            comparisonSizePercent={
+              ((pack.gzip - comparisonGzip) / comparisonGzip) * 100
+            }
+          />
+        ))}
+        <SimilarPackageCard category={category} isEmpty />
+      </div>
+    </div>
+  )
+}
+
+export default SimilarPackagesSection
