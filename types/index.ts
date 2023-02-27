ce601c043a880f6a2414b4ae4211ac6a1aeb75b3
Tianning Li <ltianningli@gmail.com>
2023-02-27 10:00:32 -0800

    Add migration

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,86 @@
 import React from 'react'
 
 export type PackageInfo = {
-  name: string
-  description: string
-  repository: string
+  isTreeShakeable?: boolean
+
+  assets: Asset[]
   dependencyCount: number
-  isTreeShakeable: boolean
+  dependencySizes: DependencySize[]
+  description: string
+  gzip: number
+  hasJSModule: string
+  hasJSNext: boolean
   hasSideEffects: string[] | boolean
+  isModuleType: boolean
+  name: string
+  parse: any
+  peerDependencies: string[]
+  repository: string
+  scoped: boolean
+  size: number
+  version: string
 }
 
+export type Asset = {
+  gzip: number
+  name: string
+  parse: any // TODO: fix type
+  size: number
+  type: string
+}
+
+export type DependencySize = {
+  approximateSize: number
+  name: string
+  isSelf?: boolean
+  percentShare?: number
+  sizeShare?: number
+}
+
+export type Tag = {
+  tag: string
+  weight: number
+}
+
+type Score = {
+  detail: {
+    maintenance: number
+    popularity: number
+    quality: number
+  }
+  final: number
+}
+
+type Maintainer = {
+  email: string
+  username: string
+}
+
+type Package = {
+  author: {
+    email: string
+    name: string
+    username: string
+  }
+  date: string
+  description: string
+  links: {
+    bugs: string
+    homepage: string
+    npm: string
+    repository: string
+  }
+  maintainers: Maintainer[]
+  name: string
+  publisher: Maintainer
+  scope: string
+  version: string
+}
+
+export type PackageSuggestion = {
+  highlight: string
+  package: Package
+  score: Score
+  searchScore: number
+}
 export type WithClassName = Pick<React.HTMLAttributes<HTMLElement>, 'className'>
