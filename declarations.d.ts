018594c2ab453854ac4fae27a3f57889f6b2042a
Shubham Kanodia <shubham.kanodia10@gmail.com>
2026-08-10 21:10:16 +0530

    fix: declare svg modules for typecheck

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,10 @@ declare module '*.scss' {
   const content: { [className: string]: string }
   export default content
 }
+
+declare module '*.svg' {
+  import type { FunctionComponent, SVGProps } from 'react'
+
+  const component: FunctionComponent<SVGProps<SVGSVGElement>>
+  export default component
+}
